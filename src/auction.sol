67acda5599c2f57c72ed86e30e49591d0c0f7efd
DVYNE <agboolafemi03@gmail.com>
2023-09-02 12:22:23 +0100

    new contract

--- src/auction.sol
+++ src/auction.sol
@@ -1,66 +1,14 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
 
-contract auction {
-          /****ERRORS */
-error notEnoughFeeToPlaceAuction();
-error youDontMeetTheCriteriaToPlaceBid();
-          /****EVENTS */
-event auctionPlaced(uint256 range, uint256 startingPrice, string name); 
-event currentBidLeader(address highestBidder, uint256 Amount); 
-        
-          /****VARIABLES */
-uint256 constant AUCTION_FEE = 0.005 ether;
-uint256 highestBid;
-address highestBidder;
-uint256 auctionStartingPrice;
-uint256 auctionRange;
-address currentHighestBidder;
-string name;
-mapping(address => uint256) otherBidders;
-
-/*****FUNCTIONS */  
+import {auction} from "src/auctionEngine.sol";
 
 
-function createAuction(uint256 auction_range, uint256 auction_starting_price, string memory auction_name ) external  payable{
-if(msg.value < AUCTION_FEE){
-    revert notEnoughFeeToPlaceAuction();
+contract placeAuction{
+ uint256 _auction_range;
+uint256 _auction_starting_price;
+string  _auction_name;
+
+    auction newAuction = new auction(_auction_range, _auction_starting_price, _auction_name) ;
+
 }
-else {
-payable(address(this)).transfer(msg.value);
-highestBid = auction_starting_price;
-highestBidder = msg.sender;
-auctionStartingPrice = auction_starting_price;
-auctionRange = auction_range;
-name = auction_name;  
-}
-}
-// would take a new bid parameter
-function bid() public payable {
-   if( (msg.value < highestBid) && ((block.timestamp > auctionRange)||(block.timestamp < auctionRange) )){
-     revert youDontMeetTheCriteriaToPlaceBid();
-   }
-   else {
-    otherBidders[highestBidder] += highestBid;
-
-    highestBid = msg.value;
-    highestBidder = msg.sender;
-
-    emit currentBidLeader(msg.sender,msg.value);
-   }  
-    
-   }
-
-function pickWinner() internal{
-    if(auctionRange > block.timestamp){
-      payable(address(this)).transfer(highestBid);
-    }
-}
-
-function returnFund() internal{
-uint totalFailedBid = otherBidders[msg.sender];
-    otherBidders[msg.sender] = 0;
-    payable(msg.sender).transfer(totalFailedBid);
-}
-}
-
---
+++ src/auctionEngine.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+
+contract auction {
+          /****ERRORS */
+error notEnoughFeeToPlaceAuction();
+error youDontMeetTheCriteriaToPlaceBid();
+error youCannotCallThisFunction();
+          /****EVENTS */
+event auctionPlaced(uint256 range, uint256 startingPrice, string name); 
+event currentBidLeader(address highestBidder, uint256 Amount); 
+        
+          /****VARIABLES */
+uint256 constant public AUCTION_FEE = 0.005 ether;
+uint256 highestBid;
+address highestBidder;
+uint256 auctionStartingPrice;
+uint256 auctionRange;
+address currentHighestBidder;
+string name;
+mapping(address => uint256) otherBidders;
+address public contractOwner;
+
+
+/*********MODIFIER   */
+modifier onlyOwner{
+  if(msg.sender != contractOwner){
+    revert youCannotCallThisFunction();
+  }
+  _;
+}
+/*****FUNCTIONS */  
+
+
+ constructor(uint256 auction_range, uint256 auction_starting_price, string memory auction_name ) payable{
+if(msg.value < AUCTION_FEE){
+    revert notEnoughFeeToPlaceAuction();
+}
+else {
+payable(address(this)).transfer(msg.value);
+highestBid = auction_starting_price;
+highestBidder = msg.sender;
+auctionStartingPrice = auction_starting_price;
+auctionRange = auction_range;
+name = auction_name;  
+contractOwner = msg.sender;
+}
+}
+// would take a new bid parameter
+function bid() public payable {
+   if( (msg.value < highestBid) && ((block.timestamp > auctionRange)||(block.timestamp < auctionRange) )){
+     revert youDontMeetTheCriteriaToPlaceBid();
+   }
+   else {
+    otherBidders[highestBidder] += highestBid;
+
+    highestBid = msg.value;
+    highestBidder = msg.sender;
+
+    emit currentBidLeader(msg.sender,msg.value);
+   }  
+    
+   }
+
+function pickWinner() public onlyOwner{
+    if(auctionRange > block.timestamp){
+      payable(address(contractOwner)).transfer(highestBid);
+    }
+}
+
+function returnFund() private{
+uint totalFailedBid = otherBidders[msg.sender];
+    otherBidders[msg.sender] = 0;
+    payable(msg.sender).transfer(totalFailedBid);
+}
+
+
+
+}
